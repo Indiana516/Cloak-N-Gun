0127a04d11d720cb10e39b9ef73c2c3e112d8ec0
BraxHarrison <AngryAxolotl23>
2018-10-11 05:07:17 -0400

    Extended menu bar and added Sprite object

---
+++ Cloak-N-Gun/js/game.js
@@ -0,0 +1,78 @@
+//code for the game is based off of code from https://developer.mozilla.org/,
+//being used as a placeholder until we get started on our game.
+function initGame() {
+  var canvas = document.getElementById("gameCanvas");
+  var context = canvas.getContext("2d");
+  var player;
+  var rightPressed = false;
+  var leftPressed = false;
+  canvas.width=800;
+  canvas.height=600;
+
+  initPlayer();
+  initControls();
+  draw();
+
+  function initPlayer(){
+    var playerImage = new Image();
+    playerImage.src = "images/play-error.png";
+    player = actor({
+      context: canvas.getContext("2d"),
+      width:20,
+      height:20,
+      xpos:600,
+      ypos:200,
+      image: playerImage
+    })
+    player.render();
+  }
+  function initControls(){
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+
+    function keyDownHandler(e) {
+      if(e.keyCode == 39) {
+        rightPressed = true;
+      }
+      else if(e.keyCode == 37) {
+        leftPressed = true;
+      }
+    }
+    function keyUpHandler(e) {
+      if(e.keyCode == 39) {
+        rightPressed = false;
+      }
+      else if(e.keyCode == 37) {
+        leftPressed = false;
+      }
+    }
+  }
+  function drawLevel(){
+    for(var i = 0; i<walls.length;i++){
+      context.beginPath();
+      context.rect(walls[i].xpos,canvas.height-walls[i].height,walls[i].width,walls[i].height);
+      context.fillStyle = "#008000";
+      context.fill();
+      context.closePath();
+    }
+  }
+  function draw(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(draw);
+    updateInputs();
+    drawLevel();
+    enforceGravity();
+    player.render();
+    player.update();
+  }
+  function updateInputs(){
+    if(rightPressed) {
+      player.move(7,0);
+    }
+    else if(leftPressed) {
+      player.move(-7,0);
+    }
+  }
+  function enforceGravity(){
+  }
+}
---
+++ Cloak-N-Gun/js/objects.js
@@ -0,0 +1,62 @@
+var walls = [{
+    xpos: 20,
+    ypos: -5,
+    width: 30,
+    height: 700,
+  },
+  {
+    xpos: 20,
+    ypos: -5,
+    width: 700,
+    height: 30,
+  }
+]
+
+function actor(options) {
+  var that = {},
+  frameIndex = 0,
+  tickCount = 0,
+  ticksPerFrame = options.ticksPerFrame || 0;
+  numberOfFrames = options.numberOfFrames ||1;
+
+  that.context = options.context;
+  that.width = options.width;
+  that.height = options.height;
+  that.xpos = options.xpos;
+  that.ypos= options.ypos;
+  that.image = options.image;
+
+  that.render = function() {
+    that.context.clearRect(0, 0, that.width, that.height);
+    that.context.drawImage(
+      that.image,
+      frameIndex * that.width / numberOfFrames,
+      0,
+      that.width / numberOfFrames,
+      that.height,
+      that.xpos,
+      that.ypos,
+      that.width / numberOfFrames,
+      that.height);
+  };
+
+  that.loop = options.loop;
+
+  that.update = function(){
+    tickCount+=1;
+    if(tickCount>ticksPerFrame){
+      tickCount=0;
+      if(frameIndex < numberOfFrames -1){
+        frameIndex +=1;
+      }
+      else if (that.loop){
+        frameIndex = 0;
+      }
+    }
+  }
+  that.move = function(xMov, yMov){
+    that.xpos = that.xpos+xMov;
+    that.ypos= that.ypos+yMov;
+  }
+  return that;
+}
